90ab34d707270df422b014e2ecb58a96cdf7b99f
prathibha101 <85889214+prathibha101@users.noreply.github.com>
2021-07-16 21:28:49 +0530

    Add files via upload

---
+++ program3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int inttype;
+	float floattype;
+	double doubletype;
+	char chartype;
+	printf("\nsize of int: %d bytes",sizeof(inttype));
+	printf("\nsize of float : %d bytes",sizeof(floattype));
+	printf("\nsize of double : %d bytes",sizeof(doubletype));
+	printf("\nsize of char: %d bytes",sizeof(chartype));
+	return 0;
+	
+	
+	
+}
