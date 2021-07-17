3d6de0181c48c58bbc4ce29fddd2335b9a58dcda
prathibha101 <85889214+prathibha101@users.noreply.github.com>
2021-07-17 10:06:44 +0530

    Add files via upload

---
+++ prog5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=1;
+	short b=3;
+	long c=5;
+	long long d=2;
+	printf("\n%d",a);
+	printf("\n%hd",b);
+	printf("\n%ld",c);
+	printf("\n%lld",d);
+	return 0;
+}
